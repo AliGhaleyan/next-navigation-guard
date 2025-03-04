36402c96d61f5b6055fb2decb9fcba3b83407d2c
AliGhaleyan <mr.ali.serjik@gmail.com>
2025-03-04 10:23:45 +0330

    feat: export default provider as NavigationGuardProvider

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {default as AppRouterProvider} from "./providers";
+export {default as NavigationGuardProvider} from "./providers";
 export {useNavigationGuard} from "./hooks/useNavigationGuard";
 export {navigationGuardHandleBeforeUnload} from "./utils";
 export {NavigationGuardContext} from "./providers/NavigationGuardProvider";
